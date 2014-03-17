52f0877a6635f788c438949d1bce9fc4c5922302
Friedrich Paetzke <f.paetzke@gmail.com>
2014-03-17 21:10:53 +0100

    Bump version: 0.0.0 → 0.0.1

--- format_sql/__init__.py
+++ format_sql/__init__.py
@@ -9,7 +9,7 @@ All rights reserved.
 from .format_sql import format_sql
 from .formatter import pretty_format
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2014 Friedrich Paetzke'
