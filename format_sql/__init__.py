c6773145a3857d99c5c171c58b3d62a7a092aefa
Friedrich Paetzke <paetzke@fastmail.fm>
2014-06-01 21:23:49 +0200

    Bump version: 0.0.2 → 0.1.0

--- format_sql/__init__.py
+++ format_sql/__init__.py
@@ -11,7 +11,7 @@ from .parser import (Comma, Compare, From, Group, Having, Identifier, Join, Key,
                      Limit, Link, Select, Statement, Sub, Where)
 from .shortcuts import format_sql
 
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2014 Friedrich Paetzke'
