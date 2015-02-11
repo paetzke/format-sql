c2fa01589d807658cca7b6732e87af905eba74b0
Friedrich Paetzke <paetzke@fastmail.fm>
2015-02-11 18:15:52 +0100

    Bump version: 0.5.0 → 0.6.0

--- format_sql/__init__.py
+++ format_sql/__init__.py
@@ -10,7 +10,7 @@ All rights reserved.
 from format_sql.parser import InvalidSQL
 from format_sql.shortcuts import format_sql
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2014 Friedrich Paetzke'
